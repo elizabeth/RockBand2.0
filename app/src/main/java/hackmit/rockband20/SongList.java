e46066aeca81856132dfe553a4809dc893b12613
luludust <elizabethduong96@gmail.com>
2016-09-17 14:24:20 -0700

    Add song list

--- app/src/main/java/hackmit/rockband20/SongList.java
+++ app/src/main/java/hackmit/rockband20/SongList.java
@@ -1,23 +1,57 @@
 package hackmit.rockband20;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class SongList extends AppCompatActivity {
+    private ArrayAdapter<String> adapter;   // holds the list of forecast to display each of
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final View decorView = getWindow().getDecorView();
 
-        getWindow().getDecorView().setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        decorView.setOnSystemUiVisibilityChangeListener
+                (new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        // Note that system bars will only be "visible" if none of the
+                        // LOW_PROFILE, HIDE_NAVIGATION, or FULLSCREEN flags are set.
+                        if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                            // TODO: The system bars are visible. Make any desired
+                            decorView.setSystemUiVisibility(
+                                    View.SYSTEM_UI_FLAG_LOW_PROFILE
+                                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+                            ActionBar actionBar = getSupportActionBar();
+                            if (actionBar != null) {
+                                actionBar.hide();
+                            }
+                        }
+                    }
+                });
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -25,6 +59,34 @@ public class SongList extends AppCompatActivity {
         }
 
         setContentView(R.layout.activity_song_list);
+
+        //controller
+        adapter = new ArrayAdapter<>(
+                this, R.layout.song_item, R.id.songItem);
+
+        //support ListView or GridView
+        AdapterView listView = (AdapterView)findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        getData();
+    }
+
+    public void getData() {
+        // clears any previous list of song data
+        adapter.clear();
+        Context context = this;
+
+        InputStream is = context.getResources().openRawResource(R.raw.songs);
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String readLine = null;
+
+        try {
+            while ((readLine = br.readLine()) != null) {
+                adapter.add(readLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
